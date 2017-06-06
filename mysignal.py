7153a93b2d120bf8540dd342c526506daaecac48
RockmanZheng <rockman-zheng@qq.com>
2017-06-06 19:52:40 +0800

    Add truncate silence effect

--- mysignal.py
+++ mysignal.py
@@ -38,11 +38,8 @@ class Signal:
         freq_step = (band_width-1)/2
         freq_supp_size = (fft_size-1)/2+1
 
-        # freq_pad_size = (freq_supp_size-band_width)%freq_step
         freq_pad_size = freq_step-freq_supp_size%freq_step+1
         num_bands = (freq_supp_size-band_width+freq_pad_size)/freq_step+1
-        # num_bands = (freq_supp_size-band_width)/freq_step+1
-        # freq_pad_size = band_width+freq_step*(num_bands-1)-freq_supp_size
         num_bands += 2
         
         # Get threshold for each frequency band
@@ -57,14 +54,11 @@ class Signal:
             band = noise_spectrum[start:start+band_width]*triang_bank
             energy = sum(map(lambda x:np.power(np.abs(x),2),band))
             thresholds.append(energy)
-            # set_trace()
 
         # Pad the original signal to its end
         time_step = (fft_size-1)/2
         pad_size = time_step-len(self.data)%time_step+1
         num_frames = (len(self.data)-fft_size+pad_size)/time_step+1
-        # num_frames = (len(self.data)-fft_size)/time_step+1
-        # pad_size = time_step*(num_frames-1)+fft_size-len(self.data)
         zeros = np.zeros(pad_size,self.dtype)
         data = np.hstack((self.data,zeros))
 
@@ -79,7 +73,6 @@ class Signal:
             start = i*time_step
             frame = data[start:start+fft_size]*hann_win
             frames.append(frame)
-        
 
         # Spectral analysis
         sharpness = 0.01
@@ -103,7 +96,6 @@ class Signal:
             # Retrieve attenuated spectrum
             new_spectrum = np.zeros(freq_supp_size,dtype=np.complex)
             for i in range(freq_supp_size-1):
-                # set_trace()
                 band_1 = bands[i/freq_step]
                 band_2 = bands[i/freq_step+1]
                 new_spectrum[i] = band_1[freq_step+i%freq_step]+band_2[i%freq_step]
@@ -115,14 +107,47 @@ class Signal:
         # Retrieve attenuated signal
         new_data = np.zeros(len(self.data))
         for i in range(len(self.data)-1):
-            # set_trace()
             frame_1 = new_frames[i/time_step]
             frame_2 = new_frames[i/time_step+1]
             new_data[i] = frame_1[time_step+i%time_step]+frame_2[i%time_step]
         new_data[len(self.data)-1] = new_frames[(len(self.data)-1)/time_step][time_step]
         self.data = np.array(new_data,dtype=self.dtype)
 
+    def truncate_silence(self,threshold):
+        fft_size = 2049
+        hann_win = get_window('hann',fft_size)
+        # Pad the original signal to its end
+        time_step = (fft_size-1)/2
+        pad_size = time_step-len(self.data)%time_step+1
+        num_frames = (len(self.data)-fft_size+pad_size)/time_step+1
+        zeros = np.zeros(pad_size,self.dtype)
+        data = np.hstack((self.data,zeros))
+
+        # Pad 2 sides of the original signal with zeros
+        zeros = np.zeros(time_step,self.dtype)
+        data = np.hstack((zeros,data,zeros))
+        num_frames += 2
+
+        # Frame signal
+        frames = []
+        for i in range(num_frames):
+            start = i*time_step
+            frame = data[start:start+fft_size]*hann_win
+            frames.append(frame)
+
+        # Truncate silence
+        new_frames = []
+        for frame in frames:
+            energy = sum(map(lambda x:np.power(np.abs(x),2),frame))
+            if energy>threshold:
+                new_frames.append(frame)
+        
+        # Retrieve truncated signal by adding up frames
+        new_data = np.zeros((len(new_frames)-1)*time_step+1)
+        for i in range(len(new_data)-1):
+            frame_1 = new_frames[i/time_step]
+            frame_2 = new_frames[i/time_step+1]
+            new_data[i] = frame_1[time_step+i%time_step]+frame_2[i%time_step]
+        new_data[len(new_data)-1] = new_frames[(len(new_data)-1)/time_step][time_step]
+        self.data = np.array(new_data,dtype=self.dtype)
             
-
-
-
